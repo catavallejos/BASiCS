4de1ba1984dd77c3aed6eebf0d3d838e768e091f
Catalina A. Vallejos <cata.vallejos.m@gmail.com>
2016-04-27 00:09:38 +0100

    version 0.5.7: (2016-04-27) - Welcome message added

---
+++ R/welcome.R
@@ -0,0 +1,8 @@
+# ==========================================================================
+# package initialization
+# ==========================================================================
+.onAttach = function(libname, pkgname) {
+  msg = "BASiCS (v0.5.7) has been extended to cover differential expression analyses (mean and over-dispersion), as described in http://genomebiology.biomedcentral.com/articles/10.1186/s13059-016-0930-3. New vignette file is in preparation. In the meantime, please refer to the supplementary files provided in the link for the code required to use the new functionalities. If you have any doubts, please contact catalina.vallejos@mrc-bsu.cam.ac.uk. "
+  msg = strwrap(msg, exdent=4, indent=4)
+  packageStartupMessage(paste(msg, collapse="\n"), appendLF=TRUE)
+}
