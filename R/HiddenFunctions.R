ffebe5bb7bccee247766d66f3ceec18b4f7cd0dd
Nils Eling <nils.eling@gmx.de>
2017-06-19 23:34:20 +0100

    Adjusted functions to SummarizedExperiment class

--- R/HiddenFunctions.R
+++ R/HiddenFunctions.R
@@ -7,10 +7,10 @@ HiddenBASiCS_MCMC_Start<-function(
   if(!is(Data,"SummarizedExperiment")) stop("'Data' is not a SummarizedExperiment class object.")
   
   # Number of instrinsic genes
-  q <- length(displayTechIndicator(Data))
-  q.bio<-sum(!displayTechIndicator(Data))
+  q <- length(rowData(Data)$Tech)
+  q.bio<-sum(!rowData(Data)$Tech)
   # Number of cells
-  n <- dim(counts(Data))[2]
+  n <- dim(assay(Data))[2]
   
   # Initialize normalization as the 'scran' estimates
   sizes.aux = c(20, 40, 60, 80, 100)
@@ -19,35 +19,35 @@ HiddenBASiCS_MCMC_Start<-function(
   if(n < 120) {sizes.aux = c(20, 40)}
   if(n < 80) {sizes.aux = c(20)}
   if(n < 40) {sizes.aux = c(10)}
-  size_scran <- scran::computeSumFactors(counts(Data, type = "biological"), sizes = sizes.aux)
+  size_scran <- scran::computeSumFactors(assay(Data)[!colData(Data)$Tech], sizes = sizes.aux)
   
-  if(length(Data@SpikeInput) > 1)
+  if(length(metadata(Data)$SpikeInput) > 1)
   {
     # Initialize s as the empirical capture efficiency rates
-    s0 = colSums(counts(Data, type = "technical")) / sum(displaySpikeInput(Data)); nu0=s0
+    s0 = colSums(assay(Data)[colData(Data)$Tech]) / sum(metadata(Data)$SpikeInput); nu0=s0
     phi0 = size_scran / s0
     phi0 = n * phi0 / sum(phi0)   
     
     # Initialize mu using average 'normalised counts' across cells
     # and true input values for spike-in genes
-    nCountsBio <- t( t(Data@Counts[!Data@Tech,]) / (phi0*s0) )
+    nCountsBio <- t( t(assay(Data)[!rowData(Data)$Tech,]) / (phi0*s0) )
     meansBio <- rowMeans( nCountsBio )
-    mu0<-c(meansBio + 1,Data@SpikeInput) # +1 to avoid zeros as starting values
+    mu0<-c(meansBio + 1,metadata(Data)$SpikeInput) # +1 to avoid zeros as starting values
   }
   else
   {
     phi0 = size_scran
     phi0 = n * phi0 / sum(phi0); 
-    for(B in unique(displayBatchInfo(Data)))
+    for(B in unique(metadata(Data)$BatchInfo))
     {
-      aux = displayBatchInfo(Data) == B
+      aux = metadata(Data)$BatchInfo == B
       phi0[aux] = sum(aux) * phi0[aux] / sum(phi0[aux])
     }
     
     nu0=phi0; s0 = NULL   
     
     # Initialize mu using average 'normalised counts' across cells
-    nCountsBio <- t( t(Data@Counts) / phi0 )
+    nCountsBio <- t( t(assay(Data)) / phi0 )
     meansBio <- rowMeans( nCountsBio )
     mu0 <- meansBio + 1 # +1 to avoid zeros as starting values    
   }
@@ -69,7 +69,7 @@ HiddenBASiCS_MCMC_Start<-function(
   # Starting values for the proposal variances
   #  ls.mu0 =  pmax(2 * log (0.02 * abs(log(mu0))),ls.mu0)
   #  ls.delta0 =  pmax(2 * log (0.02 * abs(log(delta0))),ls.delta0)
-  if(length(Data@SpikeInput) > 1) {ls.mu0 = rep(ls.mu0, q)}
+  if(length(metadata(Data)$SpikeInput) > 1) {ls.mu0 = rep(ls.mu0, q)}
   else{ls.mu0 = rep(ls.mu0, q.bio)}
   ls.delta0 = rep(ls.delta0, q.bio)
   ls.phi0 = ifelse(n<200, pmax(2*log(n),ls.phi0), 11) # 6
@@ -116,7 +116,7 @@ HiddenVarDecomp <- function(Data, object)
   if(!is(object,"BASiCS_Chain")) stop("'object' is not a BASiCS_Chain class object.")
   
   N = nrow(object@delta); q.bio = ncol(object@delta)
-  UniqueBatch = unique(Data@BatchInfo)
+  UniqueBatch = unique(metadata(Data)$BatchInfo)
   nBatch = length(UniqueBatch)
   
   if(nBatch > 1) {Theta = apply(object@theta, 1, median)}
@@ -136,8 +136,8 @@ HiddenVarDecomp <- function(Data, object)
   {
     for(Batch in 1:nBatch)
     {
-      PhiSBatch = apply(object@phi[, Data@BatchInfo == UniqueBatch[Batch]] *
-                          object@s[, Data@BatchInfo == UniqueBatch[Batch]], 1, median)
+      PhiSBatch = apply(object@phi[, metadata(Data)$BatchInfo == UniqueBatch[Batch]] *
+                          object@s[, metadata(Data)$BatchInfo == UniqueBatch[Batch]], 1, median)
       Aux = (1/(PhiSBatch * object@mu[,1:q.bio])) + object@delta *(object@theta[,Batch]+1)
       TechVarBatch[,,Batch] = object@theta[,Batch] / ( Aux + object@theta[,Batch] )
       BioVarBatch[,,Batch] = (object@delta * (object@theta[,Batch] + 1)) / (Aux + object@theta[,Batch])
