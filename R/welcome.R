4f9eee59bcec81498efdf7438f8b70d319e2c52f
catavallejos <cata.vallejos.m@gmail.com>
2017-08-16 14:17:52 +0300

    Change in welcome message

--- R/welcome.R
+++ R/welcome.R
@@ -3,7 +3,7 @@
 # ==========================================================================
 .onAttach = function(libname, pkgname) {
   #msg = "BASiCS (v0.5.7 onwards) has been extended to cover differential expression analyses (mean and over-dispersion), as described in http://genomebiology.biomedcentral.com/articles/10.1186/s13059-016-0930-3. New vignette file is in preparation. In the meantime, please refer to the supplementary files provided in the link for the code required to use the new functionalities. If you have any doubts, please contact cnvallej@uc.cl. "
-  msg = "BASiCS now (v1.0.0 onwards) utilizes the SummarizedExperiment class to collect the dataset information: expression counts, information on spike-ins and batch information. For more information use vignette('BASiCS'). \n 
+  msg = "BASiCS now (v1.0.0 onwards) utilizes the SingleCellExperiment class to collect the dataset information: expression counts, information on spike-ins and batch information. For more information use vignette('BASiCS'). \n 
   In preparation to the Bioconductor submission, multiple functions have been modified. \n
   We aimed to minimise changes that affect usage syntax, but some were required to improve functionality. \n
   For a summary of the changes, please visit: https://github.com/catavallejos/BASiCS/wiki."
