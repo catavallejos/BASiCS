94d3f045332dda06a60c134bbc82c4f55d36b35f
catavallejos <cata.vallejos.m@gmail.com>
2017-09-19 02:25:20 +0100

    version 0.99.2: (2017-09-18) - 'newBASiCS_Data': format + checks moved to `HiddenChecksBASiCS_Data` function - `HiddenChecksBASiCS_Data`: use of colMeans2, rowMeans2 - Format in welcome message

--- R/welcome.R
+++ R/welcome.R
@@ -3,12 +3,15 @@
 # ==============================================================
 .onAttach = function(libname, pkgname) {
   msg = "BASiCS now (v0.99.0 onwards) utilizes the SingleCellExperiment class \n 
-  to collect the dataset information: expression counts, information on spike-ins \n 
-  and batch information. For more information use vignette('BASiCS'). \n 
-  In preparation to the Bioconductor submission, multiple functions have been modified. \n
-  We aimed to minimise changes that affect usage syntax, \n 
-  but some were required to improve functionality. \n
-  For a summary of the changes, please visit: https://github.com/catavallejos/BASiCS/wiki."
-  msg = strwrap(msg, exdent=4, indent=4)
+  to collect the dataset information: expression counts, information on \n 
+  spike-ins and batch information. For more information use \n 
+  vignette('BASiCS'). In preparation to the Bioconductor submission, multiple \n 
+  functions have been modified. We aimed to minimise changes that affect \n 
+  usage syntax, but some were required to improve functionality. For a \n 
+  summary of the changes, please visit: \n
+  https://github.com/catavallejos/BASiCS/wiki."
+  
+  msg <- strwrap(msg, exdent=4, indent=4)
+  
   packageStartupMessage(paste(msg, collapse="\n"), appendLF=TRUE)
 }
