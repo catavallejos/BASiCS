7b53e56917f33a5ad2175c91371e369b42cdb0ca
Nils Eling <nils.eling@gmx.de>
2017-10-16 12:08:37 +0300

    Updated annotation

--- R/newBASiCS_Chain.R
+++ R/newBASiCS_Chain.R
@@ -1,26 +1,41 @@
+
 #' @title Creates a BASiCS_Chain object from pre-computed MCMC chains
 #'
 #' @description \code{BASiCS_Chain} creates a \code{\linkS4class{BASiCS_Chain}} 
 #' object from pre-computed MCMC chains.
 #'
-#' @param mu MCMC chain for gene-specific mean expression parameters 
-#' \eqn{\mu_i}, defined as true input molecules in case of technical genes
-#' (matrix with \code{q} columns, technical genes located at the end of the 
-#' matrix, all elements must be positive numbers)
-#' @param delta MCMC chain for gene-specific biological over-dispersion 
-#' parameters \eqn{\delta_i}, biological genes only
-#' (matrix with \code{q.bio} columns, all elements must be positive numbers)
-#' @param phi MCMC chain for cell-specific mRNA content normalising parameters 
+#' @param parameters List of matrices containing MCMC chains for each model 
+#' parameter. 
+#' \describe{
+##' \item{mu}{MCMC chain for gene-specific mean expression parameters 
+#' \eqn{\mu_i}, biological genes only 
+#' (matrix with \code{q.bio} columns, all elements must be positive numbers)}
+#' \item{delta}{MCMC chain for gene-specific biological over-dispersion 
+#' parameters \eqn{\delta_i}, biological genes only 
+#' (matrix with \code{q.bio} columns, all elements must be positive numbers)}
+#' \item{phi}{MCMC chain for cell-specific mRNA content normalisation parameters 
 #' \eqn{\phi_j} (matrix with \code{n} columns, all elements must be positive 
-#' numbers and the sum of its elements must be equal to \code{n})
-#' @param s MCMC chain for cell-specific technical normalisation parameters 
-#' \eqn{s_j} (matrix with \code{n} columns, all elements must be 
-#' positive numbers)
-#' @param nu MCMC chain for cell-specific random effects \eqn{\nu_j}
-#' (matrix with \code{n} columns, all elements must be positive numbers)
-#' @param theta MCMC chain for technical variability hyper-parameter 
-#' \eqn{\theta} (vector, all elements must be positive)
-#'
+#' numbers and the sum of its elements must be equal to \code{n})}
+#' \item{s}{MCMC chain for cell-specific technical normalisation parameters 
+#' \eqn{s_j} (matrix with \code{n} columns, 
+#' all elements must be positive numbers)}
+#' \item{nu}{MCMC chain for cell-specific random effects \eqn{\nu_j}
+#' (matrix with \code{n} columns, all elements must be positive numbers)}
+#' \item{theta}{MCMC chain for technical over-dispersion parameter(s) 
+#' \eqn{\theta} (matrix, all elements must be positive, 
+#' each colum represents 1 batch)}
+#' \item{\code{beta}}{MCMC chain for regression parameters
+#' (matrix with \code{k} columns where k represent the number of chosen basis functions) }
+#' \item{\code{sigma2}}{MCMC chain for the variance used during regression
+#' (matrix with one column, sigma2 represents a gloabl parameter)}
+#' \item{\code{eta}}{MCMC chain for the degrees of freedom used for regression
+#' (matrix with one column)}
+#' \item{\code{lambda}}{MCMC chain for the gene-specific random effect on the regression variance
+#' (matrix with \code{q} columns)}
+#' \item{\code{epsilon}}{MCMC chain for the gene specific regression residue (mean corrected vraribility)
+#' (matrix with \code{q} columns)}
+#' }
+#' 
 #' @return An object of class \code{\linkS4class{BASiCS_Chain}}.
 #'
 #' @examples
@@ -34,14 +49,22 @@
 #' ChainS <- displayChainBASiCS(Chain, 's')
 #' ChainNu <- displayChainBASiCS(Chain, 'nu')
 #' ChainTheta <- displayChainBASiCS(Chain, 'theta')
+#' ChainBeta <- displayChainBASiCS(Chain, 'beta')
+#' ChainSigma2 <- displayChainBASiCS(Chain, 'sigma2')
+#' ChainEta <- displayChainBASiCS(Chain, 'eta')
+#' ChainLambda <- displayChainBASiCS(Chain, 'lambda')
+#' ChainEpsilon <- displayChainBASiCS(Chain, 'epsilon')
 #'
-#' ChainNew <- newBASiCS_Chain(mu = ChainMu, delta = ChainDelta,
+#' ChainNew <- newBASiCS_Chain(parameters = list(mu = ChainMu, delta = ChainDelta,
 #'                             phi = ChainPhi, s = ChainS, 
-#'                             nu = ChainNu, theta = ChainTheta)
+#'                             nu = ChainNu, theta = ChainTheta,
+#'                             beta = ChainBeta, sigma2 = ChainSigma2,
+#'                             eta = ChainEta, lambda = ChainLambda,
+#'                             epsilon = ChainEpsilon))
 #'
 #' @seealso \code{\link[BASiCS]{BASiCS_Chain}}
 #'
-#' @author Catalina A. Vallejos \email{cnvallej@@uc.cl}
+#' @author Catalina A. Vallejos \email{cnvallej@@uc.cl}, Nils Eling
 #'
 #' @references 
 #' 
