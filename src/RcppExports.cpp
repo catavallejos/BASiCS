e1ed95cb534c9b6f3bf53dabb1107695c489ee3d
Catalina Vallejos <cata.vallejos.m@gmail.com>
2017-11-08 12:09:10 +0000

    version 1.1.2: (2017-11-08) - Skeleton for sampler with integrated out s

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -51,6 +51,50 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// HiddenBASiCS_Cata_MCMCcpp
+Rcpp::List HiddenBASiCS_Cata_MCMCcpp(int N, int Thin, int Burn, NumericMatrix Counts, NumericMatrix BatchDesign, NumericVector muSpikes, NumericVector mu0, NumericVector delta0, NumericVector phi0, NumericVector nu0, NumericVector theta0, double s2mu, double adelta, double bdelta, double s2delta, double prior_delta, NumericVector aphi, double as, double bs, double atheta, double btheta, double ar, NumericVector LSmu0, NumericVector LSdelta0, double LSphi0, NumericVector LSnu0, NumericVector LStheta0, NumericVector sumByCellAll, NumericVector sumByCellBio, NumericVector sumByGeneAll, NumericVector sumByGeneBio, int StoreAdapt, int EndAdapt, int PrintProgress);
+RcppExport SEXP _BASiCS_HiddenBASiCS_Cata_MCMCcpp(SEXP NSEXP, SEXP ThinSEXP, SEXP BurnSEXP, SEXP CountsSEXP, SEXP BatchDesignSEXP, SEXP muSpikesSEXP, SEXP mu0SEXP, SEXP delta0SEXP, SEXP phi0SEXP, SEXP nu0SEXP, SEXP theta0SEXP, SEXP s2muSEXP, SEXP adeltaSEXP, SEXP bdeltaSEXP, SEXP s2deltaSEXP, SEXP prior_deltaSEXP, SEXP aphiSEXP, SEXP asSEXP, SEXP bsSEXP, SEXP athetaSEXP, SEXP bthetaSEXP, SEXP arSEXP, SEXP LSmu0SEXP, SEXP LSdelta0SEXP, SEXP LSphi0SEXP, SEXP LSnu0SEXP, SEXP LStheta0SEXP, SEXP sumByCellAllSEXP, SEXP sumByCellBioSEXP, SEXP sumByGeneAllSEXP, SEXP sumByGeneBioSEXP, SEXP StoreAdaptSEXP, SEXP EndAdaptSEXP, SEXP PrintProgressSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< int >::type N(NSEXP);
+    Rcpp::traits::input_parameter< int >::type Thin(ThinSEXP);
+    Rcpp::traits::input_parameter< int >::type Burn(BurnSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type Counts(CountsSEXP);
+    Rcpp::traits::input_parameter< NumericMatrix >::type BatchDesign(BatchDesignSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type muSpikes(muSpikesSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type mu0(mu0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type delta0(delta0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type phi0(phi0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type nu0(nu0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type theta0(theta0SEXP);
+    Rcpp::traits::input_parameter< double >::type s2mu(s2muSEXP);
+    Rcpp::traits::input_parameter< double >::type adelta(adeltaSEXP);
+    Rcpp::traits::input_parameter< double >::type bdelta(bdeltaSEXP);
+    Rcpp::traits::input_parameter< double >::type s2delta(s2deltaSEXP);
+    Rcpp::traits::input_parameter< double >::type prior_delta(prior_deltaSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type aphi(aphiSEXP);
+    Rcpp::traits::input_parameter< double >::type as(asSEXP);
+    Rcpp::traits::input_parameter< double >::type bs(bsSEXP);
+    Rcpp::traits::input_parameter< double >::type atheta(athetaSEXP);
+    Rcpp::traits::input_parameter< double >::type btheta(bthetaSEXP);
+    Rcpp::traits::input_parameter< double >::type ar(arSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type LSmu0(LSmu0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type LSdelta0(LSdelta0SEXP);
+    Rcpp::traits::input_parameter< double >::type LSphi0(LSphi0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type LSnu0(LSnu0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type LStheta0(LStheta0SEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type sumByCellAll(sumByCellAllSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type sumByCellBio(sumByCellBioSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type sumByGeneAll(sumByGeneAllSEXP);
+    Rcpp::traits::input_parameter< NumericVector >::type sumByGeneBio(sumByGeneBioSEXP);
+    Rcpp::traits::input_parameter< int >::type StoreAdapt(StoreAdaptSEXP);
+    Rcpp::traits::input_parameter< int >::type EndAdapt(EndAdaptSEXP);
+    Rcpp::traits::input_parameter< int >::type PrintProgress(PrintProgressSEXP);
+    rcpp_result_gen = Rcpp::wrap(HiddenBASiCS_Cata_MCMCcpp(N, Thin, Burn, Counts, BatchDesign, muSpikes, mu0, delta0, phi0, nu0, theta0, s2mu, adelta, bdelta, s2delta, prior_delta, aphi, as, bs, atheta, btheta, ar, LSmu0, LSdelta0, LSphi0, LSnu0, LStheta0, sumByCellAll, sumByCellBio, sumByGeneAll, sumByGeneBio, StoreAdapt, EndAdapt, PrintProgress));
+    return rcpp_result_gen;
+END_RCPP
+}
 // HiddenBASiCS_MCMCcppReg
 Rcpp::List HiddenBASiCS_MCMCcppReg(int N, int Thin, int Burn, NumericMatrix Counts, NumericMatrix BatchDesign, NumericVector muSpikes, NumericVector mu0, NumericVector delta0, NumericVector phi0, NumericVector s0, NumericVector nu0, NumericVector theta0, double s2mu, NumericVector aphi, double as, double bs, double atheta, double btheta, double ar, NumericVector LSmu0, NumericVector LSdelta0, double LSphi0, NumericVector LSnu0, NumericVector LStheta0, NumericVector sumByCellAll, NumericVector sumByCellBio, NumericVector sumByGeneAll, NumericVector sumByGeneBio, int StoreAdapt, int EndAdapt, int PrintProgress, int k, NumericVector m0, NumericMatrix V0, double sigma2_a0, double sigma2_b0, NumericVector beta0, double sigma20, double eta0, NumericVector lambda0, double const& variance);
 RcppExport SEXP _BASiCS_HiddenBASiCS_MCMCcppReg(SEXP NSEXP, SEXP ThinSEXP, SEXP BurnSEXP, SEXP CountsSEXP, SEXP BatchDesignSEXP, SEXP muSpikesSEXP, SEXP mu0SEXP, SEXP delta0SEXP, SEXP phi0SEXP, SEXP s0SEXP, SEXP nu0SEXP, SEXP theta0SEXP, SEXP s2muSEXP, SEXP aphiSEXP, SEXP asSEXP, SEXP bsSEXP, SEXP athetaSEXP, SEXP bthetaSEXP, SEXP arSEXP, SEXP LSmu0SEXP, SEXP LSdelta0SEXP, SEXP LSphi0SEXP, SEXP LSnu0SEXP, SEXP LStheta0SEXP, SEXP sumByCellAllSEXP, SEXP sumByCellBioSEXP, SEXP sumByGeneAllSEXP, SEXP sumByGeneBioSEXP, SEXP StoreAdaptSEXP, SEXP EndAdaptSEXP, SEXP PrintProgressSEXP, SEXP kSEXP, SEXP m0SEXP, SEXP V0SEXP, SEXP sigma2_a0SEXP, SEXP sigma2_b0SEXP, SEXP beta0SEXP, SEXP sigma20SEXP, SEXP eta0SEXP, SEXP lambda0SEXP, SEXP varianceSEXP) {
@@ -169,6 +213,7 @@ END_RCPP
 
 static const R_CallMethodDef CallEntries[] = {
     {"_BASiCS_HiddenBASiCS_MCMCcpp", (DL_FUNC) &_BASiCS_HiddenBASiCS_MCMCcpp, 35},
+    {"_BASiCS_HiddenBASiCS_Cata_MCMCcpp", (DL_FUNC) &_BASiCS_HiddenBASiCS_Cata_MCMCcpp, 34},
     {"_BASiCS_HiddenBASiCS_MCMCcppReg", (DL_FUNC) &_BASiCS_HiddenBASiCS_MCMCcppReg, 41},
     {"_BASiCS_HiddenBASiCS_MCMCcppNoSpikes", (DL_FUNC) &_BASiCS_HiddenBASiCS_MCMCcppNoSpikes, 37},
     {"_BASiCS_HiddenBASiCS_DenoisedRates", (DL_FUNC) &_BASiCS_HiddenBASiCS_DenoisedRates, 7},
