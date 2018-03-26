a74213a84c1bcc4904afb78a08f8d8d4e07bd13e
nilseling <nils.eling@gmx.de>
2018-03-26 16:53:13 +0100

    Adjusted unit tests for changes TestDE function

--- tests/testthat/test_differential_test_reg.R
+++ tests/testthat/test_differential_test_reg.R
@@ -17,7 +17,7 @@ test_that("Differential test is correct (regression case)",
   FreqMean <- as.vector(table(Test$TableMean$ResultDiffMean))
   expect_that(all.equal(FreqMean, FreqMean0), is_true())
             
-  FreqDisp0 <- c(158, 166)
+  FreqDisp0 <- c(26, 158, 166)
   FreqDisp <- as.vector(table(Test$TableDisp$ResultDiffDisp))
   expect_that(all.equal(FreqDisp, FreqDisp0), is_true())
   
@@ -31,7 +31,7 @@ test_that("Differential test is correct (regression case)",
   ProbMean <- round(Test$TableMean$ProbDiffMean[1:5], 2)
   expect_that(all.equal(ProbMean, ProbMean0), is_true())
             
-  ProbDisp0 <- c(0.81, 0.72, 0.72, 0.72, 0.59)
+  ProbDisp0 <- c(0.72, 0.72, 0.72, 1.00, 0.59)
   ProbDisp <- round(tail(Test$TableDisp$ProbDiffDisp, 5), 2)
   expect_that(all.equal(ProbDisp, ProbDisp0), is_true())
   
@@ -44,7 +44,7 @@ test_that("Differential test is correct (regression case)",
   Lfc2Mean <- round(Test$TableMean$MeanLog2FC[1:5], 2)
   expect_that(all.equal(Lfc2Mean, Lfc2Mean0), is_true())
             
-  Lfc2Disp0 <- c(1.19, 0.92, 1.02, 0.86, 0.26)
+  Lfc2Disp0 <- c(0.92, 1.02, 0.86, 6.23, 0.26)
   Lfc2Disp <- round(tail(Test$TableDisp$DispLog2FC, 5),2)
   expect_that(all.equal(Lfc2Disp, Lfc2Disp0), is_true())
             
