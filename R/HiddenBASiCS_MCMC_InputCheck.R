3c9db8a63bcc4a062aa4a2b0e9f984b87de3e0ee
Nils Eling <nils.eling@gmx.de>
2019-03-15 10:43:10 +0100

    version 1.4.3 (2019-03-15)

--- R/HiddenBASiCS_MCMC_InputCheck.R
+++ R/HiddenBASiCS_MCMC_InputCheck.R
@@ -2,7 +2,7 @@ HiddenBASiCS_MCMC_InputCheck <- function(Data, N, Thin,
                                          Burn, Regression, WithSpikes)
 {
   if (!is(Data, "SingleCellExperiment")) 
-    stop("'Data' is not a SingleCellExperiment class object.")
+    stop("'Data' is not a SingleCellExperiment class object.\n")
   
   # The following checks are only relevant when the input data was
   # not created using the `newBASiCS_Data` function
@@ -10,7 +10,7 @@ HiddenBASiCS_MCMC_InputCheck <- function(Data, N, Thin,
   # Check if `Data` contains more than one type of types
   if( (length(SingleCellExperiment::spikeNames(Data)) > 1))
     stop("'Data' contains more than one type of spike-ins but only one is allowed. \n",
-         "Please remove unwanted spike-in types using. See help(isSpike). ")
+         "Please remove unwanted spike-in types using. See help(isSpike). \n")
     
   # If SpikeInput slot is missing and WithSpikes == TRUE
   
@@ -18,34 +18,34 @@ HiddenBASiCS_MCMC_InputCheck <- function(Data, N, Thin,
   if(!is.null(Spikes) & WithSpikes & 
      is.null(metadata(Data)$SpikeInput))
     stop("'Data' does not contain the 'SpikeInput' slot. \n",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation")
+         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
   
   # If isSpike slot is missing and WithSpikes == TRUE
   if(is.null(Spikes) & WithSpikes)
     stop("'Data' does not contain a logical vector to indicate \n", 
          "technical spike-in genes. Please indicate in 'isSpike(Data)' which \n",
          "genes are spike-ins or set 'WithSpikes = FALSE' \n.",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation")
+         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
   
   # If isSpike slot does not contain spikes and WithSpikes == TRUE
   if(sum(Spikes) == 0 & WithSpikes)
     stop("'isSpike(Data)' does not contain TRUE values, meaning the sce object \n",
          "does not contain spikes. Please indicate in 'isSpike(Data)' which \n",
          "genes are spike-ins or set 'WithSpikes = FALSE' \n.",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation")
+         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
   
   # If BatchInfo slot is missing and WithSpikes == FALSE
   if(!WithSpikes & is.null(colData(Data)$BatchInfo))
     stop("'Data' does not contain a BatchInfo vector needed when 'WithSpikes = FALSE'. \n", 
          "Please assign the batch information to: 'colData(Data)$BatchInfo = BatchInfo'. \n",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation")
+         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
   
   # Checking how counts are stored
   if(!("counts" %in% assayNames(Data)))
     stop("'Data' does not contain a 'counts' slot. \n",
          "Please make sure to include the raw data in the \n", 
          "SingleCellExperiment object under the name 'counts'. \n",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation")
+         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
   
   # Further checks on input 
   
@@ -56,13 +56,13 @@ HiddenBASiCS_MCMC_InputCheck <- function(Data, N, Thin,
   if (length(errors) > 0) stop(errors) 
   
   if (!(length(N) == 1 | length(Thin) == 1 | length(Burn) == 1)) 
-    stop("Invalid parameter values.")
+    stop("Invalid parameter values.\n")
   if (!(N%%Thin == 0 & N >= max(4, Thin))) 
-    stop("Please use an integer value for N (N>=4); multiple of thin.")
+    stop("Please use an integer value for N (N>=4); multiple of thin.\n")
   if (!(Thin%%1 == 0 & Thin >= 2)) 
-    stop("Please use an integer value for Thin (Thin>=2).")
+    stop("Please use an integer value for Thin (Thin>=2).\n")
   if (!(Burn%%Thin == 0 & Burn < N & Burn >= 1)) 
-    stop("Please use an integer value for Burn (1<=Burn<N); multiple of thin.")
+    stop("Please use an integer value for Burn (1<=Burn<N); multiple of thin.\n")
   if (!is.logical(Regression)) 
-    stop("Please use a logical value for the Regression parameter.")  
+    stop("Please use a logical value for the Regression parameter.\n")  
 }
--- R/HiddenChecksBASiCS_Data.R
+++ R/HiddenChecksBASiCS_Data.R
@@ -9,17 +9,23 @@ HiddenChecksBASiCS_Data <- function(Counts,
   errors <- character()
   
   if (!(is.numeric(Counts) & all(Counts >= 0) & sum(!is.finite(Counts)) == 0)) 
-    errors <- c(errors, "Invalid value for 'Counts'")
+    errors <- c(errors, "Invalid value for 'Counts'\n")
   
   if (sum(Counts%%1) > 0) 
-    errors <- c(errors, "Invalid 'Counts' (must be positive integers)")
+    errors <- c(errors, "Invalid 'Counts' (must be positive integers)\n")
   
   if (!(is.null(Tech)) & !(is.logical(Tech))) 
-    errors <- c(errors, "Invalid value for 'Tech'")
+    errors <- c(errors, "Invalid value for 'Tech'\n")
   
   if (!(is.null(SpikeInput)) & !(is.numeric(SpikeInput) & all(SpikeInput > 0) & 
         sum(!is.finite(SpikeInput)) == 0)) 
-    errors <- c(errors, "Invalid value for 'SpikeInput'.")
+    errors <- c(errors, "Invalid value for 'SpikeInput'.\n")
+  
+  # Checks on nameing of batches and cells
+  if (!is.null(BatchInfo) & sum(grepl("[[:space:]]", BatchInfo)) != 0){
+    errors <- c(errors, "The BatchInfo vector contains names with spaces.
+                Please remove any spaces from batch names.\n")
+  }
   
   q <- nrow(Counts)
   if(WithSpikes){
@@ -34,20 +40,20 @@ HiddenChecksBASiCS_Data <- function(Counts,
   if (WithSpikes) 
   {
     if (!(length(Tech) == q & sum(!Tech) == q.bio)) 
-      errors <- c(errors, "Spike-in assignment is not compatible with data.")
+      errors <- c(errors, "Spike-in assignment is not compatible with data.\n")
     
     if (sum(matrixStats::colSums2(Counts[Tech, ]) == 0) > 0) 
       errors <- c(errors, "Some cells have zero reads mapping back to the 
-                  spike-in genes. Please remove these before running the MCMC.")
+                  spike-in genes. Please remove these before running the MCMC.\n")
     
     if (sum(matrixStats::colSums2(Counts[!Tech, ]) == 0) > 0) 
       errors <- c(errors, "Some cells have zero reads mapping back to the 
-                  intrinsic genes. Please remove them before running the MCMC.")
+                  intrinsic genes. Please remove them before running the MCMC.\n")
     
     if (!(sum(Tech[seq_len(q.bio)]) == 0 & 
           sum(Tech[seq(q.bio + 1, q)]) == q - q.bio)) 
       errors <- c(errors, "Expression counts are not in the right format 
-                  (spike-in genes must be at the bottom of the matrix).")
+                  (spike-in genes must be at the bottom of the matrix).\n")
   } 
   else 
   {
@@ -55,29 +61,29 @@ HiddenChecksBASiCS_Data <- function(Counts,
     
     if (sum(matrixStats::colSums2(Counts) == 0) > 0) 
       errors <- c(errors, "Some cells have zero reads mapping back to the 
-                  intrinsic genes. Please remove them before running the MCMC.")
+                  intrinsic genes. Please remove them before running the MCMC.\n")
     
     if (length(unique(BatchInfo)) == 1) 
       errors <- c(errors, "If spike-in genes are not available, BASiCS 
                   requires the data to contain at least 2 batches of cells 
-                  (for the same population)")
+                  (for the same population)\n")
   }
   
   # Checks valid for any data
   if (!is.null(Tech) & length(Tech) != q) 
-    errors <- c(errors, "Argument's dimensions are not compatible.")
+    errors <- c(errors, "Argument's dimensions are not compatible.\n")
   
   if (length(GeneNames) != q) 
-    errors <- c(errors, "Incorrect length of GeneNames.")
+    errors <- c(errors, "Incorrect length of GeneNames.\n")
   
   if (sum(matrixStats::rowSums2(Counts) == 0) > 0) 
     warning("Some genes have zero counts across all cells. \n",
             "If comparing 2 groups, use `PriorDelta = 'log-normal' in BASiCS_MCMC.\n",
-            "If not, please remove those genes.")
+            "If not, please remove those genes.\n")
   
   if (!is.null(BatchInfo) & length(BatchInfo) != n) 
     errors <- c(errors, "BatchInfo slot is not compatible with the number of 
-                cells contained in Counts slot.")
+                cells contained in Counts slot.\n")
   
   return(errors)
 }
