48695c19e1aecec0f3f6f307633bdd4f755bb8fa
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-03-25 09:36:43 +0000

    example plot bug

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -6,3 +6,9 @@ setGeneric("displaySummaryBASiCS",
            function(object, ...) standardGeneric("displaySummaryBASiCS"))
 setGeneric("BASiCS_showFit", function(object, ...) 
   standardGeneric("BASiCS_showFit"))
+setGeneric("BASiCS_diagHist", function(object, ...) {
+  standardGeneric("BASiCS_diagHist")
+})
+setGeneric("BASiCS_diagPlot", function(object, ...) {
+  standardGeneric("BASiCS_diagPlot")
+})
