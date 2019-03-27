77115b61c4abfa774c3179a17cde45087db371f6
catavallejos <cata.vallejos.m@gmail.com>
2019-03-27 09:40:02 +0000

    version 1.5.13 (2019-03-27) - `BASiCS_D_TestDE` from deprecated to defunct (follows BioC guidelines)

--- R/BASiCS_D_TestDE.R
+++ R/BASiCS_D_TestDE.R
@@ -1,31 +1,21 @@
-#' @name BASiCS_D_TestDE
+#' @name BASiCS-defunct
+#' @aliases BASiCS_D_TestDE
 #' 
-#' @title Detection of genes with changes in expression.
+#' @title Defunct functions in package \sQuote{BASiCS}
 #' 
-#' @description This function is no longer in use and will 
-#' be removed from future releases of \code{BASiCS}. 
-#' Please use \code{\link[BASiCS]{BASiCS_TestDE}} instead.
+#' @description These functions are defunct and no longer available. 
 #' 
-#' @param ... Optional parameters.
+#' @details These functions are defunct and no longer available. 
 #' 
-#' @return This function is no longer in use and will 
-#' be removed from future releases of \code{BASiCS}. 
-#' Please use \code{\link[BASiCS]{BASiCS_TestDE}} instead.
-#'  
-#' @examples
-#' 
-#' # Use 'BASiCS_TestDE' instead
-#' help(BASiCS_TestDE)
-#' 
-#' @details This function is no longer in use and will 
-#' be removed from future releases of \code{BASiCS}.
+#'   \itemize{
+#'   \item{BASiCS_D_TestDE: \code{\link{BASiCS_TestDE}}}
+#'   }
 #' 
 #' @author Catalina A. Vallejos \email{cnvallej@@uc.cl}
 #' 
-#' @rdname BASiCS_D_TestDE
-#' @export
+#' @rdname BASiCS-defunct
 BASiCS_D_TestDE <- function(...) {
   
-    .Deprecated("BASiCS_TestDE")
+  .Defunct("BASiCS_TestDE")
 
 }
