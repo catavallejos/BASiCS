667085c42b9bc7d26efc543196495901bb4913ec
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-03-28 16:41:57 +0000

    Move plots, try in case something fails

---
+++ R/BASiCS_diagHist.R
@@ -0,0 +1,85 @@
+#' @title Create diagnostic plots of MCMC parameters
+#'
+#' @description Plot a histogram of effective sample size or Geweke's diagnostic
+#' z-statistic. See \link[coda]{effectiveSize} and \link[coda]{geweke.diag} for
+#' more details.
+#'
+#' @param object an object of class \code{\linkS4class{BASiCS_Summary}}
+#' @param Param Optional name of a chain parameter to restrict the histogram;
+#' if not supplied, all parameters will be assessed.
+#' Possible values: \code{'mu'}, \code{'delta'}, \code{'phi'},
+#' \code{'s'}, \code{'nu'}, \code{'theta'}, \code{'beta'},
+#' \code{'sigma2'} and \code{'epsilon'}.
+#'
+#' @return A ggplot object.
+#'
+#' @examples
+#'
+#' # See
+#' help(BASiCS_MCMC)
+#'
+#' @seealso \code{\linkS4class{BASiCS_Chain}}
+#'
+#' @author Alan O'Callaghan \email{a.b.ocallaghan@sms.ed.ac.uk}
+#'
+#' @export
+BASiCS_diagHist <- function(object, Param = NULL) {
+  if (!inherits(object, "BASiCS_Chain")) {
+    stop(paste0("Incorrect class for object:", class(object)))
+  }
+  Measure <- "effectiveSize"
+
+  if (is.null(Param)) {
+    metric <- lapply(names(object@parameters), function(param) {
+      try(HiddenGetMeasure(object, param, Measure), silent = TRUE)
+    })
+    ind_error <- vapply(metric, function(x) inherits(x, "try-error"), logical(1))
+    metric <- metric[!ind_error]
+    if (all(ind_error)) {
+      stop("coda::effectiveSize failed for all parameters.")
+    } else {
+      warning(
+        paste("coda::effectiveSize failed for some parameters:",
+          paste(names(object@parameters)[ind_error], collapse = ", ")
+        )
+      )
+    }
+    metric <- Reduce(c, metric)              
+  } else {
+    metric <- HiddenGetMeasure(object, Param, Measure)
+  }
+  if (length(metric) == 1) {
+    stop(paste0("Cannot produce histogram of a single value (", metric, ")"))
+  }
+  ggplot2::ggplot(mapping = ggplot2::aes(x = metric)) + 
+    ggplot2::geom_histogram(bins = grDevices::nclass.FD(metric)) +
+    ggplot2::labs(x = HiddenScaleName(Measure, Param),
+                  y = "Count")
+}
+
+HiddenScaleName <- function(Measure = c("effectiveSize",
+                                  "geweke.diag"),
+                      Param = NULL) {
+  Measure <- match.arg(Measure)
+  measure_name <- switch(Measure, 
+    effectiveSize = "Effective sample size",
+    geweke.diag = "Geweke diagnostic"
+  )
+  if (!is.null(Param)) {
+    measure_name <- paste0(measure_name, ": ", Param)
+  }
+  measure_name
+}
+
+HiddenGetMeasure <- function(object, 
+                       Param,
+                       Measure = c("effectiveSize",
+                                   "geweke.diag")) {
+  Measure <- match.arg(Measure)
+  MeasureFun <- match.fun(Measure)
+  metric <- MeasureFun(coda::mcmc(HiddenGetParam(object, Param)))
+  if (Measure == "geweke.diag") {
+    metric <- metric$z
+  }
+  metric
+}
