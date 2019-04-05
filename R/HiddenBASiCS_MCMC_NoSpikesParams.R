e0aacc86be2daf8270c98dab424a3edb05a08410
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-04-05 15:43:49 +0100

    Simplify constrain check

--- R/HiddenBASiCS_MCMC_NoSpikesParams.R
+++ R/HiddenBASiCS_MCMC_NoSpikesParams.R
@@ -10,8 +10,7 @@ HiddenBASiCS_MCMC_NoSpikesParam <- function(Counts, ConstrainType,
     ConstrainGene <- seq_len(q.bio) - 1
     NotConstrainGene <- 0
     NonZero <- which(matrixStats::rowSums2(Counts) > 0)
-  }
-  if (ConstrainType == 2) {
+  } else if (ConstrainType == 2) {
     ConstrainGene <- which(matrixStats::rowSums2(Counts) >= 1) - 1
     NotConstrainGene <- which(matrixStats::rowSums2(Counts) < 1) - 1
     NonZero <- ConstrainGene + 1
@@ -23,7 +22,7 @@ HiddenBASiCS_MCMC_NoSpikesParam <- function(Counts, ConstrainType,
   # the nearest 10% genes located around the constrain
 
   if (StochasticRef) {
-    aux.ref <- cbind(ConstrainGene, abs(log(mu0[ConstrainGene+1]) - Constrain))
+    aux.ref <- cbind(ConstrainGene, abs(log(mu0[ConstrainGene + 1]) - Constrain))
     aux.ref <- aux.ref[order(aux.ref[, 2]), ]
     # In total ConstrainProp*100% of genes to be used as reference candidates
     CandidateRef <- round(ConstrainProp * q.bio)
@@ -31,15 +30,12 @@ HiddenBASiCS_MCMC_NoSpikesParam <- function(Counts, ConstrainType,
     # generaed by makeExample_BASiCS function (less than 200 genes)
     if (length(ConstrainGene) > CandidateRef) {
       RefGenes <- aux.ref[seq_len(CandidateRef), 1]
-    }
-    else {
+    } else {
       RefGenes <- aux.ref[, 1]
     }
     RefGene <- RefGenes[1]
-  }
-  else {
-    aux.ref <- which(abs(log(mu0[ConstrainGene+1]) - Constrain) ==
-                       min(abs(log(mu0[ConstrainGene+1]) - Constrain)))[1]
+  } else {
+    aux.ref <- which.min(abs(log(mu0[ConstrainGene + 1]) - Constrain))
     RefGene <- ConstrainGene[aux.ref]
     RefGenes <- RefGene
   }
