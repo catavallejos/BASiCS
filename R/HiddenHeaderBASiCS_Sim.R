03df1f11cc5e92c9e265dfc26b18b98b77e282a9
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-04-10 11:13:47 +0100

    Formatting changes, no logic

--- R/HiddenHeaderBASiCS_Sim.R
+++ R/HiddenHeaderBASiCS_Sim.R
@@ -8,14 +8,16 @@ HiddenHeaderBASiCS_Sim <- function(Mu, Mu_spikes, Delta, Phi,
   # Data dimensions - total number of genes
   if(!is.null(Mu_spikes)) {
     q <- q.bio + length(Mu_spikes)
+  } else {
+    q <- q.bio
   }
-  else q <- q.bio
   # Data dimensions - number of batches
   if(!is.null(BatchInfo)) {
     nBatch <- length(unique(BatchInfo))  
+  } else {
+    nBatch <- 1
   }
-  else nBatch <- 1
-  
+
   if (!all(c(n, nBatch, q, q.bio) > 0)) {
     stop("Arguments' dimensions are not valid")
   }
@@ -36,30 +38,31 @@ HiddenHeaderBASiCS_Sim <- function(Mu, Mu_spikes, Delta, Phi,
   }
   
   # Basic checks for parameters that are only present when spike-ins are there
-  if(!is.null(Mu_spikes)) {
-    if (!(is.vector(Mu_spikes) & is.numeric(Mu_spikes) & all(Mu_spikes > 0))) { 
-      stop("Invalid argument value for 'Mu_spikes'.")
-    }
+  if (!is.null(Mu_spikes) &&
+      (!(is.vector(Mu_spikes) & 
+        is.numeric(Mu_spikes) & 
+        all(Mu_spikes > 0)))) { 
+    stop("Invalid argument value for 'Mu_spikes'.")
   }
-  if(!is.null(Phi)) {
-    if (!(is.vector(Phi) & is.numeric(Phi) & all(Phi > 0) & (length(Phi) == n) &
-          isTRUE(all.equal(sum(Phi), n, tolerance = 0.01)))) {
-      stop("Invalid argument value for 'Phi'.")
-    }
+  if (!is.null(Phi) &&
+      (!(is.vector(Phi) & 
+        is.numeric(Phi) & 
+        all(Phi > 0) & 
+        (length(Phi) == n) &
+        isTRUE(all.equal(sum(Phi), n, tolerance = 0.01))))) {
+    stop("Invalid argument value for 'Phi'.")
   }
-  if(!is.null(BatchInfo)) {
-    if(!(length(BatchInfo) == n)) {
-      stop("Invalid argument value for 'BatchInfo'. Must be same length as 'S'")
-    }
+  if (!is.null(BatchInfo) && !(length(BatchInfo) == n)) {
+    stop("Invalid argument value for 'BatchInfo'. Must be same length as 'S'")
   }
   
   # Warnings when only some of the optional arguments are provided
-  if(is.null(Mu_spikes) + is.null(Phi) == 1) {
-    if(is.null(Mu_spikes)) {
+  if (is.null(Mu_spikes) + is.null(Phi) == 1) {
+    if (is.null(Mu_spikes)) {
       Phi <- NULL
       warning("Because 'Phi = NULL', 'Mu_spikes' will be ignored")
     }
-    if(is.null(Phi)) {
+    if (is.null(Phi)) {
       Mu_spikes = NULL
       warning("Because 'Mu_spikes = NULL', 'Phi' will be ignored")
     }
@@ -67,16 +70,14 @@ HiddenHeaderBASiCS_Sim <- function(Mu, Mu_spikes, Delta, Phi,
   
   # Check that batches are present if spike-ins not included
   # Potentially remove this in next releases
-  if(is.null(Mu_spikes)) {
-    if(is.null(BatchInfo)) {
+  if (is.null(Mu_spikes)) {
+    if (is.null(BatchInfo)) {
       stop("When spike-ins are not included, 'BatchInfo' is required.")
-    }
-    else {
-      if(length(unique(BatchInfo)) <= 1) {
+    } else {
+      if (length(unique(BatchInfo)) <= 1) {
         stop("When spike-ins are not included, 'BatchInfo' must contain",
              " multiple batches (i.e. length(unique(BatchInfo)) > 1).")  
-      }  
+      }
     }
   }
-  
 }
