195531d8e3d784840437caca0d4ad63b98a46feb
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-10-04 15:37:42 +0100

    Add colnames test

---
+++ tests/testthat/test_data.R
@@ -0,0 +1,11 @@
+test_that("data preserves colnames", {
+  # Expression counts
+  set.seed(1)
+  Counts <- matrix(rpois(50*10, 2), ncol = 10)
+  rownames(Counts) <- paste0('Gene', 1:50)
+  colnames(Counts) <- paste('Cell', seq_len(ncol(Counts)))
+  BatchInfo <- c(rep(1, 5), rep(2, 5))
+  # Creating a BASiCS_Data object (with batch effect)
+  DataExample <- newBASiCS_Data(Counts, BatchInfo = BatchInfo)
+  expect_identical(colnames(DataExample), colnames(Counts))
+})
