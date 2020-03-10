1a7d1b0b457c54d0433782a4e18607f5d6bdc860
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2020-03-10 18:32:32 +0000

    Format and roxy

--- R/utils_MCMC.R
+++ R/utils_MCMC.R
@@ -5,43 +5,54 @@
                                     Regression,
                                     WithSpikes) {
 
-  if (!is(Data, "SingleCellExperiment")) 
+  if (!is(Data, "SingleCellExperiment")) {
     stop("'Data' is not a SingleCellExperiment class object.\n")
+  }
   
   # The following checks are only relevant when the input data was
   # not created using the `newBASiCS_Data` function
 
   # Check if `Data` contains more than one type of types
   if ((length(SingleCellExperiment::altExpNames(Data)) > 1)) {
-    stop("'Data' contains multiple 'altExp' assays but only one is allowed. \n",
-         "The latter should include information for spike-in genes. \n",
-         "Please remove unwanted the remaining elements. See help(altExp). \n")
+    stop(
+      "'Data' contains multiple 'altExp' assays but only one is allowed. \n",
+      "The latter should include information for spike-in genes. \n",
+      "Please remove unwanted the remaining elements. See help(altExp). \n"
+    )
   }
   
   if (WithSpikes & length(altExpNames(Data)) > 0) {
-    message("altExp '", altExpNames(Data),"' is assumed to contain spike-in genes.\n",
-            "see help(altExp) for details. \n")  
+    message(
+      "altExp '", altExpNames(Data),"' is assumed to contain spike-in genes.\n",
+      "see help(altExp) for details. \n"
+    )
   }
   
   # If SpikeInput slot is missing and WithSpikes == TRUE
   if ((length(altExpNames(Data)) > 0) & WithSpikes & is.null(metadata(Data)$SpikeInput)) {
-    stop("'Data' does not contain 'SpikeInput' as metadata. \n",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
+    stop(
+      "'Data' does not contain 'SpikeInput' as metadata. \n",
+      "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n"
+    )
   }
   
   # If isSpike slot is missing and WithSpikes == TRUE
   if((length(altExpNames(Data)) == 0)  & WithSpikes) {
-    stop("'Data' does not contain information about spike-in genes \n", 
-         "Please indicate include this information using 'altExp' \n",
-         "or set 'WithSpikes = FALSE' \n.",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
+    stop(
+      "'Data' does not contain information about spike-in genes \n", 
+      "Please indicate include this information using 'altExp' \n",
+      "or set 'WithSpikes = FALSE' \n.",
+      "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n"
+    )
   }
     
   # If BatchInfo slot is missing and WithSpikes == FALSE
   if(!WithSpikes & is.null(colData(Data)$BatchInfo)) {
-    stop("'Data' does not contain a BatchInfo vector needed when 'WithSpikes = FALSE'. \n", 
-         "Please assign the batch information to: 'colData(Data)$BatchInfo = BatchInfo'. \n",
-         "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n")
+    stop(
+      "'Data' does not contain a BatchInfo vector needed when 'WithSpikes = FALSE'. \n", 
+      "Please assign the batch information to: 'colData(Data)$BatchInfo = BatchInfo'. \n",
+      "See: https://github.com/catavallejos/BASiCS/wiki/2.-Input-preparation\n"
+    )
   }
 
   # Checking how counts are stored
@@ -56,18 +67,20 @@
   # Further checks on input
   errors <- .ChecksBASiCS_Data(Data, WithSpikes)
   
-  if (length(errors) > 0) stop(errors) 
+  if (length(errors) > 0) {
+    stop(errors) 
+  }
   
   if (!(length(N) == 1 | length(Thin) == 1 | length(Burn) == 1)) {
     stop("Invalid parameter values.\n")
   }
-  if (!(N%%Thin == 0 & N >= max(4, Thin))) {
+  if (!(N %% Thin == 0 & N >= max(4, Thin))) {
     stop("Please use an integer value for N (N>=4); multiple of thin.\n")
   }
-  if (!(Thin%%1 == 0 & Thin >= 2)) {
+  if (!(Thin %% 1 == 0 & Thin >= 2)) {
     stop("Please use an integer value for Thin (Thin>=2).\n")
   }
-  if (!(Burn%%Thin == 0 & Burn < N & Burn >= 1)) {
+  if (!(Burn %% Thin == 0 & Burn < N & Burn >= 1)) {
     stop("Please use an integer value for Burn (1<=Burn<N); multiple of thin.\n")
   }
   if (!is.logical(Regression)) {
