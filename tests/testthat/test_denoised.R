98b99d75b286379674053261b43d6c0a1b5b4eb4
Catalina Vallejos <catalina.vallejos@ed.ac.uk>
2023-03-07 21:05:03 +0000

    version 2.11.9 (2023-03-07) - Fixes minor bug in test_denoised.R

--- tests/testthat/test_denoised.R
+++ tests/testthat/test_denoised.R
@@ -1,13 +1,6 @@
 test_that("DenoisedCounts and Rates require matching dimensions", {
   data <- makeExampleBASiCS_Data()
-  chain <- run_MCMC(
-    data,
-    N = 10,
-    Thin = 2,
-    Burn = 4,
-    Regression = FALSE,
-    WithSpikes = TRUE
-  )
+  data(ChainSC)
   expect_error(
     BASiCS_DenoisedCounts(data, ChainSC),
     "Chain and Data are different dimensions"
