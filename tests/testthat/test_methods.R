845dc233c173218550d6634a78f9e770504092f7
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-03-11 17:04:06 +0000

    Remove test

--- tests/testthat/test_methods.R
+++ tests/testthat/test_methods.R
@@ -20,7 +20,6 @@ test_that("Summary na.rm", {
   expect_true(all(is.na(s2@parameters$nu[1, ])))
   expect_true(all(is.na(s2@parameters$nu[4, ])))
   ChainSC@parameters$nu[1, 1:ncol(ChainSC@parameters$nu)] <- NA
-  expect_error(s3 <- Summary(ChainSC, na.rm = TRUE))
 })
 
 test_that("subset", {
